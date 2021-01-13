02e894b0f73c2912bf08a6698925a9efd090ca5b
David Onuh <davidonuh1@gmail.com>
2021-01-13 23:22:29 +0100

    production ready

--- testsite/asgi.py
+++ testsite/asgi.py
@@ -1,21 +1,27 @@
 import os
 
 import django
+from channels.routing import get_default_application
+
 from channels.http import AsgiHandler
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import chat.routing
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testsite.settings')
+
 django.setup()
 
 
+from channels.auth import AuthMiddlewareStack
+import testsite.routing
+
+
+
+
 application = ProtocolTypeRouter({
-    'http': AsgiHandler(),
+    #'http': AsgiHandler(),
     'websocket': AuthMiddlewareStack(
         URLRouter(
-            chat.routing.websocket_urlpatterns
+            testsite.routing.websocket_urlpatterns
         )
     ),
 })
