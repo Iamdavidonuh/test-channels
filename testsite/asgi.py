78cf28909aff9894fa5fb4ca21ec1bb3e1dbcab2
David Onuh <davidonuh1@gmail.com>
2021-01-13 20:46:24 +0100

    add base route to all websockets and update asgi

--- testsite/asgi.py
+++ testsite/asgi.py
@@ -2,7 +2,9 @@ import os
 
 import django
 from channels.http import AsgiHandler
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testsite.settings')
@@ -11,4 +13,9 @@ django.setup()
 
 application = ProtocolTypeRouter({
     'http': AsgiHandler(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
