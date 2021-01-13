4f849bbda95e6f0103faa0c1bc3b546296f33afd
David Onuh <davidonuh1@gmail.com>
2021-01-13 22:38:03 +0100

    correct variable mispelling in consumers

--- chat/consumers.py
+++ chat/consumers.py
@@ -5,13 +5,13 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
 
-        def disconnect(self, close_code):
-            pass
+    def disconnect(self, close_code):
+        pass
 
-        def receive(self, text_data):
-            text_data_json = json.load(text_data)
-            message = text_data['message']
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
 
-            self.send(text_data=json.dumps({
-                'message': message
-            }))
+        self.send(text_data=json.dumps({
+            'message': message
+        }))
